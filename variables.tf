6614bb159f66c4227a4a159221b595a4daa04c34
Nipun Bahri <bahri.nipun@gmail.com>
2022-01-20 16:29:12 +0100

    dssd

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ARM_CLIENT_ID" {
+  default = "64e91fff-3e3f-42ec-b85e-5e1c24791d93"
+}
+
+variable "ARM_CLIENT_SECRET" {
+    default = 1
+}
+
+variable "ARM_TENANT_ID" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaVp7TBD+qngQYPFiMzYhW68zyJRl1TmUh+S/8Gc/Hg8bLnRbWBjQ75YY/C2BxaTBcYCX8fd4dEGku/jf9hkcGM8SukwXg4qssCVjr3KKIoPa3QNfmH2uW0QCdXs+TR9j5bo1iaZ2hjgVa87QJ9SiW/bmcNETk5/PLWX6jilW9+Vj1MsHve5IZ7vHdkkiHe7PtkVglzTGbWb/V6DAhhr+6Vwny+W45Juczq0ScJiQg+YWc4A1OAj+GcyU4nntoAZN/BIvBK9KR5+pldOklnujsUrQSH/uicv/Rw2bxyN/xLXR2im6fIUaptRKxz1muKw3TFbKfJqRtTkeB5fVrRM4F nipun@cc-bd44156f-75946bd9b5-tklwn"
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+    default = "testaks9"
+}
