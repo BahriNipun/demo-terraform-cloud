d546ad531bda88e6203d75ca2352119513eba9b7
Nipun Bahri <bahri.nipun@gmail.com>
2022-01-20 16:02:37 +0100

    asd

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "k8s" {
+    name     = var.resource_group_name
+    location = var.location
+}
